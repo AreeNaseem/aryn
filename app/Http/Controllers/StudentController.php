40e83d1a839f13d59cffa27b3501903c7409974f
athik.13 <athik.13@gmail.com>
2022-09-24 22:35:56 +0500

    Test

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class StudentController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -63,10 +63,10 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show(Student $student)
     {
         return view('show', compact('student'));
     }
@@ -74,10 +74,10 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit(Student $student)
     {
         return view('edit', compact('student'));
     }
@@ -86,10 +86,10 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Student $student)
     {
         $request->validate([
             'student_name'      =>  'required',
@@ -99,8 +99,7 @@ class ProductController extends Controller
 
         $student_image = $request->hidden_student_image;
 
-        if($request->student_image != '')
-        {
+        if ($request->student_image != '') {
             $student_image = time() . '.' . request()->student_image->getClientOriginalExtension();
 
             request()->student_image->move(public_path('images'), $student_image);
@@ -123,13 +122,12 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(Student $student)
     {
         $student->delete();
-
         return redirect()->route('students.index')->with('success', 'Student Data deleted successfully');
     }
 }
